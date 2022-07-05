5353cc7df84d53e9ebeeec90e418e3bcc151aac0
Petrus Isaksson <48841014+petruspsyko@users.noreply.github.com>
2022-07-05 14:43:05 +0200

    Add files via upload

---
+++ task4_kommenterat.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  5 10:42:10 2022
+@author: petrusisaksson
+"""
+from numpy import *
+from matplotlib.pyplot import *
+
+def fast_approx_ln(x,n):
+    a_0 = (1+x)/2                  
+    g_0 = sqrt(x)
+    A=zeros([n,n]) # skapar n*n-matris med nollor
+    A[0,0]=a_0 # stoppar in a_0 på första platsen (k,i)
+    d=a_0 # def d
+    
+    for i in range(1,n+1): # vi hoppar över element(0,0)           
+        a_0 = (a_0+g_0)/2           
+        g_0 = sqrt(a_0 + g_0) # här produceras a_0+i, vilket heter a_0
+        A[0,i]=a_0 # lägg till a_i på plats i 
+        
+        for k in range(1,n+1): # för elementen i första raden av A
+            d=(((a_0+g_0)/2)-(4**-k)*a_0)/1-4**-k
+            A[i,k]=d # lägger till d
+    
+    return (x-1)/A[n,n]
