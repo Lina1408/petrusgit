dd1890a8662c8bad046a5a6e0b2de126bdb45030
Petrus Isaksson <48841014+petruspsyko@users.noreply.github.com>
2022-07-05 16:55:06 +0200

    Update task4_kommenterat.py

--- task4_kommenterat.py
+++ task4_kommenterat.py
@@ -11,15 +11,14 @@ def fast_approx_ln(x,n):
     g_0 = sqrt(x)
     A=zeros([n,n]) # skapar n*n-matris med nollor
     A[0,0]=a_0 # stoppar in a_0 på första platsen (k,i)
-    d=a_0 # def d
     
-    for i in range(1,n+1): # vi hoppar över element(0,0)           
+    for i in range(1,n): # vi hoppar över element(0,0)           
         a_0 = (a_0+g_0)/2           
         g_0 = sqrt(a_0 + g_0) # här produceras a_0+i, vilket heter a_0
         A[0,i]=a_0 # lägg till a_i på plats i 
-        
-        for k in range(1,n+1):
-            d=(A[i-1,k]-(4**-k)*A[i-1,k-1])/1-4**-k
-            A[i,k]=d # lägger till d
     
-    return (x-1)/A[n,n]
+    for i in range(1,n)
+        for k in range(1,n):
+            A[i,k]=(A[k-1,i]-((4**-k)*A[k-1,i-1]))/(1-(4**-k))
+    
+    return (x-1)/A[n-1,n-1]
